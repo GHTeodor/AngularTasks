c87da680057aafb79f1d83d8260c0ec96f135848
GHTeodor <Theodor.Lviv@gmail.com>
2022-09-20 15:15:31 +0300

    Task №1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'task1', pathMatch: 'full' },
+  { path: 'task1', loadChildren: () => import('./modules').then(m => m.Task1Module) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
